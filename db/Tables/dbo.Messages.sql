a62d69b835737a14ce6eda84048b4b0ccd342ec2
Tugberk Ugurlu <tugberk@outlook.com>
2015-02-06 14:19:44 +0000

    added foreign keys and made the foreign key nullable for ReceiverId

--- db/Tables/dbo.Messages.sql
+++ db/Tables/dbo.Messages.sql
@@ -2,10 +2,14 @@ CREATE TABLE [dbo].[Messages]
 (
 [Id] [int] NOT NULL,
 [SenderId] [int] NOT NULL,
-[ReceiverId] [int] NOT NULL,
+[ReceiverId] [int] NULL,
 [Content] [nvarchar] (max) COLLATE Latin1_General_CI_AS NOT NULL,
 [ReceivedOn] [datetimeoffset] NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+ALTER TABLE [dbo].[Messages] ADD
+CONSTRAINT [FK__Messages__Receiv__182C9B23] FOREIGN KEY ([ReceiverId]) REFERENCES [dbo].[Users] ([Id])
+ALTER TABLE [dbo].[Messages] ADD
+CONSTRAINT [FK__Messages__Sender__173876EA] FOREIGN KEY ([SenderId]) REFERENCES [dbo].[Users] ([Id])
 GO
 ALTER TABLE [dbo].[Messages] ADD CONSTRAINT [Id] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
 GO
