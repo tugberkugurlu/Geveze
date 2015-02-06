a0b70f1571a51a03fb34ad0f87b9e5e6b0777371
Tugberk Ugurlu <tugberk@outlook.com>
2015-02-06 14:47:04 +0000

    no default ids

--- db/Tables/dbo.Messages.sql
+++ db/Tables/dbo.Messages.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Messages]
 (
-[Id] [uniqueidentifier] NOT NULL CONSTRAINT [DF__Messages__Id__2A4B4B5E] DEFAULT (newsequentialid()),
+[Id] [uniqueidentifier] NOT NULL,
 [SenderId] [uniqueidentifier] NOT NULL,
 [ReceiverId] [uniqueidentifier] NULL,
 [Content] [nvarchar] (max) COLLATE Latin1_General_CI_AS NOT NULL,
@@ -9,9 +9,12 @@ CREATE TABLE [dbo].[Messages]
 ALTER TABLE [dbo].[Messages] ADD 
 CONSTRAINT [PK_Messages] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
 ALTER TABLE [dbo].[Messages] ADD
-CONSTRAINT [FK__Messages__Receiv__2C3393D0] FOREIGN KEY ([ReceiverId]) REFERENCES [dbo].[Users] ([Id])
+CONSTRAINT [FK__Messages__Receiv__31EC6D26] FOREIGN KEY ([ReceiverId]) REFERENCES [dbo].[Users] ([Id])
 ALTER TABLE [dbo].[Messages] ADD
-CONSTRAINT [FK__Messages__Sender__2B3F6F97] FOREIGN KEY ([SenderId]) REFERENCES [dbo].[Users] ([Id])
+CONSTRAINT [FK__Messages__Sender__30F848ED] FOREIGN KEY ([SenderId]) REFERENCES [dbo].[Users] ([Id])
+
+
+
 
 
 GO
--- db/Tables/dbo.Users.sql
+++ db/Tables/dbo.Users.sql
@@ -1,8 +1,9 @@
 CREATE TABLE [dbo].[Users]
 (
-[Id] [uniqueidentifier] NOT NULL CONSTRAINT [DF__Users__Id__239E4DCF] DEFAULT (newsequentialid()),
+[Id] [uniqueidentifier] NOT NULL,
 [Name] [nvarchar] (200) COLLATE Latin1_General_CI_AS NOT NULL
 ) ON [PRIMARY]
 ALTER TABLE [dbo].[Users] ADD 
 CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
+
 GO
