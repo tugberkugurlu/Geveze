801e38214edf6a1a35c8a429497957e9bb75acae
Tugberk Ugurlu <tugberk@outlook.com>
2015-03-20 11:33:39 +0000

    updated the scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,10 +4,14 @@ param(
     $buildTarget = "/t:Default"
 )
 
+dir "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\"
+
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
 
 & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget /p:VisualStudioVersion=12.0
 & "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\sqlci.exe" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
+
+dir "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\"
 
 dir $artifactsPath
