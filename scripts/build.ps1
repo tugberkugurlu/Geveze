079d61429c87e1a537f7efa998888b5925c2f726
Tugberk Ugurlu <tugberk@outlook.com>
2015-03-20 11:03:22 +0000

    fixed the stuff :smile:

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -7,7 +7,13 @@ param(
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
 
-& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget /p:VisualStudioVersion=12.0
+# & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget /p:VisualStudioVersion=12.0
+
+Write-Host "testing $env:TEMP\sqlci\"
+Write-Host Test-Path "$env:TEMP\sqlci\"
+Write-Host "testing $env:TEMP\sqlci\sqlci.exe"
+Write-Host Test-Path "$env:TEMP\sqlci\sqlci.exe"
+
 & "$env:TEMP\sqlci\sqlci.exe" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
 
 dir $artifactsPath
