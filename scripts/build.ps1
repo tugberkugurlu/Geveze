28b3d578e58d10233b2eb55160df8cd2ee608109
Tugberk Ugurlu <tugberk@outlook.com>
2015-03-20 11:43:20 +0000

    updated the scripts..

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,13 +4,15 @@ param(
     $buildTarget = "/t:Default"
 )
 
+Write-Host "dir folders"
+dir "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 dir "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\"
 
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
 
 & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget /p:VisualStudioVersion=12.0
-& "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\sqlci.exe" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
+& "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\sqlCI.exe" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
 
 dir "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\sqlci\"
 
