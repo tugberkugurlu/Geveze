c7de5bf4320396728f2fbe57e27e1878d1dfcf2a
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 16:00:41 +0000

    remove old start command

--- scripts/pre-build.ps1
+++ scripts/pre-build.ps1
@@ -2,5 +2,4 @@
 
 New-Item "c:\dlmas" -ItemType Directory | Out-Null
 Invoke-WebRequest "https://download.red-gate.com/DLMAutomationSuite.exe" -OutFile "c:\dlmas\DLMAutomationSuite.exe"
-& start /wait C:\dlmas\DLMAutomationSuite.exe /install /quiet /norestart
 Start-Process -FilePath C:\dlmas\DLMAutomationSuite.exe -ArgumentList @("/install", "/quiet", "/norestart") -NoNewWindow -Wait
