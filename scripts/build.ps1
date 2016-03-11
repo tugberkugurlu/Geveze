ad19807f0f417650b433978d847820261761e87d
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 16:17:05 +0000

    moar changes

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,9 +6,11 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-$DLMAS_HOME = [System.Environment]::GetEnvironmentVariable("DLMAS_HOME")
+dir "C:\Program Files (x86)\Red Gate\DLM Automation Suite 1\"
+
+$DLMAS_HOME = [System.Environment]::GetEnvironmentVariable("DLMAS_HOME", "Machine")
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
 
 & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget /p:VisualStudioVersion=12.0
-& "$(Join-Path $DLMAS_HOME 'sqlci\sqlCI.exe')" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
+& "$(Join-Path $DLMAS_HOME 'SQLCI\sqlCI.exe')" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
