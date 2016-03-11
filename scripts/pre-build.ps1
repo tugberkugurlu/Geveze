7d5550a66a1fe58e7bd4a2ad6353e7d37ae4d43e
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 15:59:34 +0000

    use Start-Process

--- scripts/pre-build.ps1
+++ scripts/pre-build.ps1
@@ -2,4 +2,5 @@
 
 New-Item "c:\dlmas" -ItemType Directory | Out-Null
 Invoke-WebRequest "https://download.red-gate.com/DLMAutomationSuite.exe" -OutFile "c:\dlmas\DLMAutomationSuite.exe"
-& start `/wait c:\dlmas\DLMAutomationSuite.exe `/install `/quiet `/norestart
+& start /wait C:\dlmas\DLMAutomationSuite.exe /install /quiet /norestart
+Start-Process -FilePath C:\dlmas\DLMAutomationSuite.exe -ArgumentList @("/install", "/quiet", "/norestart") -NoNewWindow -Wait
