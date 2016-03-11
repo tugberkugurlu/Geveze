123b7acc26b5f7136d68c6846c7ee38916b82a05
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 16:10:13 +0000

    getting DLMAS_HOME env var

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,8 +6,9 @@ param(
 
 $ErrorActionPreference = "Stop"
 
+$DLMAS_HOME = [System.Environment]::GetEnvironmentVariable("DLMAS_HOME")
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
 
 & "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $buildFile $buildParams $buildTarget /p:VisualStudioVersion=12.0
-& "$(Join-Path $env:DLMAS_HOME 'sqlci\sqlCI.exe')" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
+& "$(Join-Path $DLMAS_HOME 'sqlci\sqlCI.exe')" Build --scriptsFolder="$dbPath" --outputFolder="$artifactsPath" --packageId="Geveze" --packageVersion="$env:APPVEYOR_BUILD_VERSION"
