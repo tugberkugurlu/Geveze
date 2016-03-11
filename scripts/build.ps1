a19d572aaf321c482d02b0f07bca47c7a50f36ca
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 16:21:28 +0000

    clean up

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,8 +6,6 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-dir "C:\Program Files (x86)\Red Gate\DLM Automation Suite 1\"
-
 $DLMAS_HOME = [System.Environment]::GetEnvironmentVariable("DLMAS_HOME", "Machine")
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
