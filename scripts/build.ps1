eaf8d7a0872a95d1f649c299a1e720cf31f9a0fb
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 16:02:55 +0000

    better $ErrorActionPreference = "Stop"

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = "Stop"
-
 param(
     $buildFile   = (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "build.msbuild"),
     $buildParams = "/p:Configuration=Release",
     $buildTarget = "/t:Default"
 )
+
+$ErrorActionPreference = "Stop"
 
 $dbPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\db")
 $artifactsPath = (join-path (join-path (Split-Path -parent $MyInvocation.MyCommand.Definition) "..") "\artifacts")
--- scripts/pre-build.ps1
+++ scripts/pre-build.ps1
@@ -1,4 +1,4 @@
- $ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Stop"
 
 New-Item "c:\dlmas" -ItemType Directory | Out-Null
 Invoke-WebRequest "https://download.red-gate.com/DLMAutomationSuite.exe" -OutFile "c:\dlmas\DLMAutomationSuite.exe"
