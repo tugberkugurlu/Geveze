4c9bd7cf4ad0af60c4f3f5be2567f96243a4b4a9
Tugberk Ugurlu <tugberk@outlook.com>
2016-03-11 15:43:03 +0000

    fix install script

--- scripts/pre-build.ps1
+++ scripts/pre-build.ps1
@@ -2,4 +2,4 @@
 
 New-Item "c:\dlmas" -ItemType Directory | Out-Null
 Invoke-WebRequest "https://download.red-gate.com/DLMAutomationSuite.exe" -OutFile "c:\dlmas\DLMAutomationSuite.exe"
-& start /wait DLMAutomationSuite.exe /install /quiet /norestart
+& start /wait c:\dlmas\DLMAutomationSuite.exe /install /quiet /norestart
